0ee4b25526238c3e2fc029c3045a1a9ff622f6c5
Josh Bressers <josh@bress.net>
2019-01-31 19:09:14 -0600

    Add a script for adding upstream repos

---
+++ add-repo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+from elasticsearch import Elasticsearch
+
+def main():
+
+    es = Elasticsearch(['http://localhost:9200'])
+
+    name = sys.argv[1]
+
+    response = requests.get("https://api.github.com/repos/%s" % name)
+    repo = response.json()
+
+    es.update(id=name, index='repos', doc_type='_doc', body={'doc' :repo, 'doc_as_upsert': True}, request_timeout=30)
+
+if __name__ == "__main__":
+    main()
