ea0faffc1e83c063142ccae06b3147f8076ab414
Josh Bressers <josh@bress.net>
2021-08-24 19:51:20 -0500

    Use ESURL and no doctype

--- add-repo.py
+++ add-repo.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import requests
 from elasticsearch import Elasticsearch
 
 def main():
 
-    es = Elasticsearch(['http://localhost:9200'])
+    if 'ESURL' not in os.environ:
+        es_url = "http://localhost:9200"
+    else:
+        es_url = os.environ['ESURL']
+
+    es = Elasticsearch([es_url])
 
     name = sys.argv[1]
 
@@ -16,7 +22,7 @@ def main():
     if 'message' in repo:
         sys.exit("Repo not found")
 
-    es.update(id=name, index='repos', doc_type='_doc', body={'doc' :repo, 'doc_as_upsert': True}, request_timeout=30)
+    es.update(id=name, index='repos', body={'doc' :repo, 'doc_as_upsert': True}, request_timeout=30)
 
 if __name__ == "__main__":
     main()
